2028c5a1a74be9c94eb5512d0ec4ea74cd9c47ba
lifetruman <oneslifetruman@gmail.com>
2022-03-21 21:41:17 +0900

    default projects

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,60 @@
+/*
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log(23);
+
+
+let firstName = "Matilda";
+let first = "jonas";
+
+let firstNamePerson;
+let first_name_person;
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+// Variable name conventions (변수 이름 규칙)
+let jonas_matilda = "JM";
+let $new = 27;
+let person = "Jonas";
+let PI = 3.1415;
+
+let myFirstJob = "Programmer";
+let myCurrentJob = "Teacher";
+
+let job1 = "programmer";
+let job2 = "teacher";
+
+console.log(myFirstJob);
+
+let country = "South of Korea";
+let continent = "Asia";
+let population = 51780000;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+*/
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof "Jonas");
+
+javascriptIsFun = "YES!";
+console.log(typeof javascriptIsFun); // boolean data type --> string data type (dynamic typing)
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 2022;
+console.log(year);
+console.log(typeof year);
+
+console.log(typeof null);
